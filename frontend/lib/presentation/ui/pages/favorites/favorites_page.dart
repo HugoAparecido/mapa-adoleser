c06a09d553f581f0c2fe3455e3df2e5e6cf36db5
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-02-24 17:04:01 -0300

    Comunicação botão e diálogo da avaliação
    Inserção na página favoritos do componente botão de avaliação e chamada da caixa de diálogo da avaliação nele.

--- frontend/lib/presentation/ui/pages/favorites/favorites_page.dart
+++ frontend/lib/presentation/ui/pages/favorites/favorites_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mapa_adoleser/core/utils/responsive_utils.dart';
 import 'package:mapa_adoleser/presentation/ui/widgets/appbar/custom_app_bar.dart';
+import 'package:mapa_adoleser/presentation/ui/widgets/avaliacao_dialog.dart';
+import 'package:mapa_adoleser/presentation/ui/widgets/custom_button.dart';
+import 'package:mapa_adoleser/presentation/ui/widgets/custom_button_avaliacao.dart';
 import 'package:mapa_adoleser/presentation/ui/widgets/drawer/custom_drawer.dart';
 import 'package:mapa_adoleser/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
@@ -19,12 +22,16 @@ class FavoritesPage extends StatelessWidget {
       endDrawer: ResponsiveUtils.shouldShowDrawer(context)
           ? CustomDrawer(isLoggedIn: isLoggedIn)
           : null,
-      body: const Center(
-        child: Text(
-          'Favoritos!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      body: Center(
+          child: AvaliacaoButton(
+        text: "Fazer Avaliação",
+        icon: Icons.square,
+        onPressed: () {
+          showDialog(context: context, builder: (BuildContext context){
+            return AvaliacaoDialog();
+          });
+        },
+      )),
     );
   }
 }
